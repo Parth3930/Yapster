292851ec61c5b1de2f88be87d1e47f1f3f858eb0
Rover <sharmaparth.developer@gmail.com>
2025-06-15 17:27:38 +0530

    vCode 9

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,7 +38,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdkVersion(23)
         targetSdk = flutter.targetSdkVersion
-        versionCode = 8
+        versionCode = 9
         versionName = flutter.versionName
     }
 
